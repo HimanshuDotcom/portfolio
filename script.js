d0113abba8aab5bd93e60d27d38ebbe760799054
Himanshu <arorahimanshu472@gmail.com>
2021-04-26 21:00:39 +0530

    nav toggler added

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 
 let navLinks = $('.nav__link');
 
+function hideSidebar() {
+    $('.sidebar').css("left", "-200px");
+    $('.nav-button').html("&rarr;");
+}
+
+function showSidebar() {
+    $('.sidebar').css("left", "0px");
+    $('.nav-button').html("&larr;");
+}
+
 navLinks.each(function(index,value){
     console.log(value)
     $(value).click(function() {
         console.log(this)
         $('.active').toggleClass('active swingHorizontal');
         $(this).toggleClass('active swingHorizontal');
+        hideSidebar();
     })
 
 })
 
 
-// $('.toggler').click(function() {
-//     $('.sidebar').toggle();
-//     // $(".container").css("grid-template-columns", "[sidenav-start ] 27rem [sidenav-end full-start] minmax(5rem, 1fr) [center-start] repeat(8,[col-start] minmax(min-content, 15rem) [col-end]) [center-end] minmax(6rem,1fr) [full-end]");
-// })
+// nav-button
+
+$('.nav-button').click(function() {
+    let element = $(this);
+    let sidebar = $('.sidebar')
+    console.log(sidebar.css("left"))
+    if(sidebar.css("left") == "0px") 
+        hideSidebar();
+    else 
+        showSidebar();
+})
